80ec987e79957a9ca67a9a6ff2fcfbf35b95ae71
v4lodya <valodyapapikyan@gmail.com>
2022-11-18 09:35:16 +0400

    feature/improvements fixes

--- controllers/user-controller/index.ts
+++ controllers/user-controller/index.ts
@@ -1,19 +1,27 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 
 class UserController {
-  constructor() {};
-  async getUsers(req: any, res: Response, next: NextFunction) {
+  constructor() {}
+  async getUser(req: any, res: Response, next: NextFunction) {
     return res.status(200).json({
       success: true,
       data: [
         {
-          name: "John",
+          name: 'John',
         },
         {
-          name: "Steve",
+          name: 'Steve',
         },
       ],
-    })
+    });
+  }
+
+  async signUp(req: any, res: Response, next: NextFunction) {
+    return res.status(200).json({ success: true });
+  }
+
+  async signIn(req: any, res: Response, next: NextFunction) {
+    return res.status(200).json({ success: true });
   }
 }
-export default new UserController()
+export default new UserController();
